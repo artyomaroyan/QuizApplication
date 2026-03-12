4069865ed5f95481f8a5e41d76d255fcc71f0eed
Artyom <aroyanart98@gmail.com>
2026-03-13 00:12:01 +0400

    refactor(quiz): introduce ConsoleQuiz and move application logic. - implement `ConsoleQuiz` class - move logic from `Main` class to `ConsoleQuiz`

--- src/main/java/org/pure/java/project/Main.java
+++ src/main/java/org/pure/java/project/Main.java
@@ -1,13 +1,10 @@
 package org.pure.java.project;
 
-import org.pure.java.project.model.Question;
 import org.pure.java.project.persistence.QuestionLoaderImpl;
 import org.pure.java.project.persistence.QuestionLoaderService;
 import org.pure.java.project.service.QuestionService;
 import org.pure.java.project.service.QuestionServiceImpl;
-
-import java.util.List;
-import java.util.Scanner;
+import org.pure.java.project.ui.ConsoleQuiz;
 
 /**
  * Author: Artyom Aroyan
@@ -16,54 +13,10 @@ import java.util.Scanner;
  */
 public class Main {
     static void main() {
-
-        Scanner scanner = new Scanner(System.in);
-        IO.println("Choose your service - (Add, Read, Exam):");
-        String input = scanner.nextLine();
-
         QuestionService questionService = new QuestionServiceImpl();
-        QuestionLoaderService loaderService = new QuestionLoaderImpl();
+        QuestionLoaderService questionLoaderService = new QuestionLoaderImpl();
 
-        switch (input.trim().toUpperCase()) {
-            case "ADD" -> questionService.save();
-            case "READ" -> {
-                IO.println("""
-                        Choose read mode:
-                        1 - Read all questions:
-                        2 - Filter by difficulty
-                        3 - Shuffle by amount and difficulty
-                        """);
-
-                String readOption = scanner.nextLine().trim();
-
-                switch (readOption) {
-                    case "1" -> {
-                        List<Question> questions = loaderService.loadAllQuestions();
-                        questions.forEach(IO::println);
-                    }
-
-                    case "2" -> {
-                        IO.println("Enter difficulty (LOW, MEDIUM, HIGH):");
-                        String difficulty = scanner.nextLine();
-                        List<Question> questions = loaderService.loadByDifficulty(difficulty);
-                        questions.forEach(IO::println);
-                    }
-                    case "3" -> {
-                        IO.println("Enter difficulty (LOW, MEDIUM, HIGH):");
-                        String difficulty = scanner.nextLine();
-
-                        IO.println("Enter number of questions:");
-                        int amount = Integer.parseInt(scanner.nextLine());
-
-                        List<Question> questions = loaderService.loadShuffleQuestions(amount, difficulty);
-                        questions.forEach(IO::println);
-                    }
-                    default -> IO.println("Unknown read option:");
-                }
-            }
-
-            case "EXAM" -> IO.println("coming soon!");
-            default -> IO.println("Unknown command:");
-        }
+        ConsoleQuiz consoleQuiz = new ConsoleQuiz(questionService, questionLoaderService);
+        consoleQuiz.view();
     }
 }
--- src/main/java/org/pure/java/project/ui/ConsoleQuiz.java
+++ src/main/java/org/pure/java/project/ui/ConsoleQuiz.java
@@ -1,9 +1,82 @@
 package org.pure.java.project.ui;
 
+import org.pure.java.project.model.Question;
+import org.pure.java.project.persistence.QuestionLoaderService;
+import org.pure.java.project.service.QuestionService;
+
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Author: Artyom Aroyan
  * Date: 16.01.26
  * Time: 00:02:36
  */
 public class ConsoleQuiz {
+    private final QuestionService questionService;
+    private final QuestionLoaderService questionLoaderService;
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public ConsoleQuiz(QuestionService questionService, QuestionLoaderService questionLoaderService) {
+        this.questionService = questionService;
+        this.questionLoaderService = questionLoaderService;
+    }
+
+    public void view() {
+        IO.println("Choose your service - (Add, Read, Exam):");
+        String input = SCANNER.nextLine();
+
+        try {
+            ServiceType serviceType = ServiceType.valueOf(input.trim().toUpperCase());
+
+            switch (serviceType) {
+                case ADD -> questionService.save();
+                case READ -> {
+                    IO.println("""
+                            Choose read mode:
+                            1 - Read all questions:
+                            2 - Filter by difficulty
+                            3 - Shuffle by amount and difficulty
+                            """);
+                    String readOption = SCANNER.nextLine().trim();
+
+                    switch (readOption) {
+                        case "1" -> {
+                            List<Question> questions = questionLoaderService.loadAllQuestions();
+                            questions.forEach(IO::println);
+                        }
+
+                        case "2" -> {
+                            IO.println("Enter difficulty (LOW, MEDIUM, HIGH):");
+                            String difficulty = SCANNER.nextLine();
+                            List<Question> questions = questionLoaderService.loadByDifficulty(difficulty);
+                            questions.forEach(IO::println);
+                        }
+
+                        case "3" -> {
+                            IO.println("Enter difficulty (LOW, MEDIUM, HIGH):");
+                            String difficulty = SCANNER.nextLine();
+                            IO.println("Enter number of questions:");
+                            int amount = Integer.parseInt(SCANNER.nextLine());
+                            List<Question> questions = questionLoaderService.loadShuffleQuestions(amount, difficulty);
+                            questions.forEach(IO::println);
+                        }
+                        default -> IO.println("Unknown read option:");
+                    }
+                }
+
+                case EXAM -> IO.println("Coming soon!");
+                default -> IO.println("Unknown command:");
+            }
+        } catch (IllegalArgumentException ex) {
+            IO.println("Unknown command:" + ex.getMessage());
+        }
+    }
+}
+
+enum ServiceType {
+    ADD,
+    READ,
+    EXAM
 }
